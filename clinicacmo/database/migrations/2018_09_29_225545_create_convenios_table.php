b6e82c41f04dc4d398a8c8f365ce6334199e9015
valdirdpg <valdirdpg.hotmail.com>
2018-10-01 01:30:56 -0300

    primeiro envio

---
+++ clinicacmo/database/migrations/2018_09_29_225545_create_convenios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConveniosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('convenios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nm_convenio');
+            $table->date('dt_inicio');
+            $table->char('ds_regioes');
+            $table->date('dt_registro');
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('convenios');
+    }
+}
---
+++ clinicacmo/database/migrations/2018_09_30_023048_create_medicos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('nm_medico',45);
+            $table->integer('cpf_medico');
+            $table->integer('rg_medico');
+            $table->integer('nr_crm');
+            $table->char('nm_usuario');
+            $table->char('senha');
+            $table->integer('num_acesso');
+            $table->integer('laudo_idlaudo');
+            $table->integer('cd_especialidade');
+            $table->date('dt_registro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
