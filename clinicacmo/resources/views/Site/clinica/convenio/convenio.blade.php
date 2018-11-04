dd79dc8c4d37e16679fbc50f16a6ea8767bdd516
valdirdpg <valdirdpg.hotmail.com>
2018-11-04 00:47:54 -0300

    Melhorias no layout do CRUD

--- clinicacmo/resources/views/Site/clinica/convenio/convenio.blade.php
+++ clinicacmo/resources/views/Site/clinica/convenio/convenio.blade.php
@@ -1,22 +1,25 @@
-@extends('Site.templates.template')
+@extends('Site.templates.listagem')
 
 @section('content')
 
 
-        <div class="panel-heading">
-            <h1 class="page-header">{{$tela ?? 'Convênio Localizado'}}</h1></div>
-    <div class="panel table-responsive col-lg-10">
+    <!--  <div class="panel-heading">
+           <h1 class="page-header">{{$tela ?? 'Convênio Localizado'}}</h1></div>-->
+
+    <!--<div class="panel table-responsive col-lg-10">
         {!! Form::open(['route' => 'convenios.search', 'class' => 'form-inline form-search'])!!}
         {!! Form::text('search', null, ['placeholder' => 'Pesquisar?', 'class' => 'form-control'])!!}
         {!! Form::submit('search', ['class' => 'btn btn-success']) !!}
-        {!! Form::close() !!}  </div>
-    <table class="table table-striped">
-        <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Situação</th>
-            <th>Ações</th>
-        </tr>
+        {!! Form::close() !!}  </div>-->
+    <table class="table table-hover">
+        <thead class="thead-light">
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Situação</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
         @foreach($convenio as $convenios)
         <tr>
             <td>{{$convenios->id}}</td>
@@ -31,9 +34,6 @@
 
     </table>
 
-    <a href="{{route('convenios.create')}}" class="btn btn-primary btn-add">
-        <span class="glyphicon glyphicon-plus"></span> Cadastrar
-    </a>
     <div>{{$convenio->links()}}</div>
 
 
--- clinicacmo/resources/views/Site/templates/template.blade.php
+++ clinicacmo/resources/views/Site/templates/template.blade.php
@@ -1,17 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!--<link rel="stylesheet" type="text/css" href="/assets/painel/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/assets/painel/css/boots.min.css">-->
-    <link rel="stylesheet" type="text/css" href="/assets/painel/css/style.css">
-    <title>{{$title}}</title>
-</head>
-    <body>
-        <div class="container">
-            @yield('content')
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+
+        <link rel="stylesheet" type="text/css" href="{{url('assets/painel/css/bootstrap-theme.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{url('assets/painel/css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{url('assets/painel/fonts/glyphicons-halflings-regular.woff')}}">
+        <link rel="stylesheet" type="text/css" href="{{url('assets/painel/css/style.css')}}">
+        <title class="">{{$title}}</title>
+    </head>
+        <body class="teste">
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Bootstrap theme</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="{{route('convenios.index')}}">Início</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+        <div class="jumbotron">
+            <div class="panel-body col-sm-offset-1 container">
+               @yield('content')
+            </div>
         </div>
 
-    </body>
-</html>
+            <script src="/assets/painel/js/jquery.min.js"></script>
+            <script src="/assets/painel/js/bootstrap.min.js"></script>
+
+        </body>
+    </html>
